b413bc20c994a4eb6eb1a15d696ab1d540686994
AlejandroMorgante <alejandro.morgante13@gmail.com>
2026-02-07 21:42:22 -0300

    chore: ignore dbt artifacts

--- airflow/include/dbt/target/compiled/airflow_agents_dbt/models/customer_summary.sql
+++
@@ -1,6 +0,0 @@
-select
-  customer_id,
-  first_name,
-  last_name,
-  is_active::boolean as is_active
-from "dbt"."analytics"."raw_customers"
--- airflow/include/dbt/target/run/airflow_agents_dbt/models/run_events.sql
+++
@@ -1,9 +0,0 @@
-
-      insert into "dbt"."analytics"."run_events" ("run_started_at", "invocation_id")
-    (
-        select "run_started_at", "invocation_id"
-        from "run_events__dbt_tmp003757242585"
-    )
-
-
-  

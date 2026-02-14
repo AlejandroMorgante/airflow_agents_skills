0183bfa485e8655b45eb9915e35548d12a2d72a3
daherakel <dahersakel@gmail.com>
2026-02-13 23:16:52 -0300

    Add dbt transformation DAG for Pokemon data
    - Use Cosmos for dbt orchestration
    - Trigger on pokemon raw data ready dataset
    - Select only pokemon-tagged models
    - Configure connection to use host.docker.internal for network compatibility

---
+++ airflow/dags/dbt_pokemon_cosmos_dag.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from airflow.datasets import Dataset
+from airflow.models import Variable
+from cosmos import DbtDag
+from cosmos.config import ProfileConfig, ProjectConfig, RenderConfig
+
+DBT_DIR = "/usr/local/airflow/include/dbt"
+POKEMON_RAW_READY = Dataset("airflow_agents://pokemon/raw_ready")
+DBT_ENV = {
+    "DBT_PROFILES_DIR": DBT_DIR,
+    # Use host.docker.internal to avoid DNS issues between containers.
+    "DBT_HOST": Variable.get("dbt_host", default_var="host.docker.internal"),
+    "DBT_PORT": Variable.get("dbt_port", default_var="5433"),
+    "DBT_USER": Variable.get("dbt_user", default_var="dbt"),
+    "DBT_PASSWORD": Variable.get("dbt_password", default_var="dbt"),
+    "DBT_DBNAME": Variable.get("dbt_dbname", default_var="dbt"),
+    "DBT_SCHEMA": Variable.get("dbt_schema", default_var="analytics"),
+    "DBT_RAW_POKEMON_SCHEMA": Variable.get("pokemon_schema", default_var="raw_pokemon"),
+}
+
+profile_config = ProfileConfig(
+    profile_name="airflow_agents_dbt",
+    target_name="dev",
+    profiles_yml_filepath=f"{DBT_DIR}/profiles.yml",
+)
+
+render_config = RenderConfig(select=["tag:pokemon"])
+
+
+dbt_pokemon_cosmos_dag = DbtDag(
+    dag_id="dbt_pokemon_cosmos_dag",
+    schedule=[POKEMON_RAW_READY],
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["dbt", "pokemon", "cosmos"],
+    project_config=ProjectConfig(
+        dbt_project_path=DBT_DIR,
+        env_vars=DBT_ENV,
+        install_dbt_deps=True,
+    ),
+    profile_config=profile_config,
+    render_config=render_config,
+)
